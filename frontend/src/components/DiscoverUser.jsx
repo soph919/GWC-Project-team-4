9ad20f424879d29c1e61a12e42630859a353b812
soph <sophiavignali@hotmail.com>
2026-04-10 12:29:51 -0400

    Fix optional chaining for image and user info

--- frontend/src/components/DiscoverUser.jsx
+++ frontend/src/components/DiscoverUser.jsx
@@ -4,10 +4,10 @@ const DiscoverUser = ({portfolio}) => {
   return (
         <div className="other-user">
                 <div className="title">
-                    <img src={portfolio?.image.secure_url} alt="avatar"/>
+                    <img src={portfolio?.image?.secure_url} alt="avatar"/>
                     <div>
                         <h3>{portfolio?.portfolio_name}</h3>
-                        <h4>{portfolio?.user_info.first_name} {portfolio?.user_info.last_name}</h4>
+                        <h4>{portfolio?.user_info?.first_name} {portfolio?.user_info?.last_name}</h4>
                     </div>
                 </div>
             <p>{portfolio?.description}</p>
